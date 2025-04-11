1284f3f4e054a0329533bf3692b36ff42822b1e7
OmkarChoughule <omkarchoughule2703@gmail.com>
2025-04-11 14:36:24 +0530

    added images to data

---
+++ add_images.py
@@ -0,0 +1,13 @@
+import json
+
+with open("pokemon_formatted_with_descriptions.json", "r") as f:
+    data = json.load(f)
+
+for i, pokemon in enumerate(data):
+    dex_number = i + 1  # if order matches dex
+    pokemon["image"] = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{dex_number}.png"
+    pokemon["shiny"] = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny{dex_number}.png"
+    
+
+with open("pokemon_data_with_images.json", "w") as f:
+    json.dump(data, f, indent=2)
